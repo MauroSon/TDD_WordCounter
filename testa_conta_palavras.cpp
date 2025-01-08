1108627d92ce1288c8fef60aaa0ea0c4d5dc6bac
mauro <mcontaalternativa@gmail.com>
2025-01-08 17:04:22 -0300

    RED: Alfabetico(Capital & Normal)

--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -85,3 +85,64 @@ TEST_CASE("Teste Alfabético(CAPITAL)", "[single-file]") {
 
     REQUIRE(contaPalavras(fileName) == expectedAswer);
 }
+
+TEST_CASE("Teste Alfabético(CAPITAL & Normal)", "[single-file]") {
+    CustomDictionary expectedAswer;
+    std::string fileName = {"inputFile4.txt"};
+
+    expectedAswer.insert("A",1);
+    expectedAswer.insert("B",1);
+    expectedAswer.insert("C",1);
+    expectedAswer.insert("D",1);
+    expectedAswer.insert("E",1);
+    expectedAswer.insert("F",1);
+    expectedAswer.insert("G",1);
+    expectedAswer.insert("H",1);
+    expectedAswer.insert("I",1);
+    expectedAswer.insert("J",1);
+    expectedAswer.insert("K",1);
+    expectedAswer.insert("L",1);
+    expectedAswer.insert("M",1);
+    expectedAswer.insert("N",1);
+    expectedAswer.insert("O",1);
+    expectedAswer.insert("P",1);
+    expectedAswer.insert("Q",1);
+    expectedAswer.insert("R",1);
+    expectedAswer.insert("S",1);
+    expectedAswer.insert("T",1);
+    expectedAswer.insert("U",1);
+    expectedAswer.insert("V",1);
+    expectedAswer.insert("W",1);
+    expectedAswer.insert("X",1);
+    expectedAswer.insert("Y",1);
+    expectedAswer.insert("Z",1);
+    expectedAswer.insert("a",1);
+    expectedAswer.insert("b",1);
+    expectedAswer.insert("c",1);
+    expectedAswer.insert("d",1);
+    expectedAswer.insert("e",1);
+    expectedAswer.insert("f",1);
+    expectedAswer.insert("g",1);
+    expectedAswer.insert("h",1);
+    expectedAswer.insert("i",1);
+    expectedAswer.insert("j",1);
+    expectedAswer.insert("k",1);
+    expectedAswer.insert("l",1);
+    expectedAswer.insert("m",1);
+    expectedAswer.insert("n",1);
+    expectedAswer.insert("o",1);
+    expectedAswer.insert("p",1);
+    expectedAswer.insert("q",1);
+    expectedAswer.insert("r",1);
+    expectedAswer.insert("s",1);
+    expectedAswer.insert("t",1);
+    expectedAswer.insert("u",1);
+    expectedAswer.insert("v",1);
+    expectedAswer.insert("w",1);
+    expectedAswer.insert("x",1);
+    expectedAswer.insert("y",1);
+    expectedAswer.insert("z",1);
+
+
+    REQUIRE(contaPalavras(fileName) == expectedAswer);
+}
