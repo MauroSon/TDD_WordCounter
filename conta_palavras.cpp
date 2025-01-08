4739613cb71f335746770c7bce996db9b1689ef5
mauro <mcontaalternativa@gmail.com>
2025-01-08 14:01:36 -0300

    sorting words

--- conta_palavras.cpp
+++ conta_palavras.cpp
@@ -4,6 +4,8 @@
 
 int contaPalavras(std::string fileName) {
     std::vector<std::string> words;
+    int qnt = 0;
+    std::string temp;
 
     std::ifstream input(fileName);
     std::string text;
@@ -13,8 +15,23 @@ int contaPalavras(std::string fileName) {
     }
     input.close();
     for (auto it = words.begin(); it  != words.end(); ++it) {
-        std::cout << *it << std::endl;
+       //std::cout << *it << std::endl;
+       ++qnt;
     }
+
+    for (int i = 0; i < qnt; ++i){
+        for (int j = i+1; j<qnt;++j){
+            if (words[i] > words[j]){
+                temp = words[i];
+                words[i] = words[j];
+                words[j] = temp;
+            };
+        };
+    };
+
+    for ( auto it = words.begin(); it != words.end(); ++it){
+        std::cout << *it << std::endl;
+    };
 
     return 0;
 }
