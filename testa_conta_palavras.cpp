3769353ad501cf5707cd1e7b659a2763525ee0c1
mauro <mcontaalternativa@gmail.com>
2025-01-08 16:30:39 -0300

    GREEN:Teste alfabetico

--- testa_conta_palavras.cpp
+++ testa_conta_palavras.cpp
@@ -5,13 +5,36 @@
 #define CATCH_CONFIG_MAIN
 #include "../TDD_WordCounter/catch.hpp"
 
-TEST_CASE("Teste Inicio", "[single-file]") {
+TEST_CASE("Teste Alfabético", "[single-file]") {
     CustomDictionary expectedAswer;
-    std::string testeInicio = {"inputFile.txt"};
+    std::string fileName = {"inputFile.txt"};
 
     expectedAswer.insert("a",1);
     expectedAswer.insert("b",1);
     expectedAswer.insert("c",1);
+    expectedAswer.insert("d",1);
+    expectedAswer.insert("e",1);
+    expectedAswer.insert("f",1);
+    expectedAswer.insert("g",1);
+    expectedAswer.insert("h",1);
+    expectedAswer.insert("i",1);
+    expectedAswer.insert("j",1);
+    expectedAswer.insert("k",1);
+    expectedAswer.insert("l",1);
+    expectedAswer.insert("m",1);
+    expectedAswer.insert("n",1);
+    expectedAswer.insert("o",1);
+    expectedAswer.insert("p",1);
+    expectedAswer.insert("q",1);
+    expectedAswer.insert("r",1);
+    expectedAswer.insert("s",1);
+    expectedAswer.insert("t",1);
+    expectedAswer.insert("u",1);
+    expectedAswer.insert("v",1);
+    expectedAswer.insert("w",1);
+    expectedAswer.insert("x",1);
+    expectedAswer.insert("y",1);
+    expectedAswer.insert("z",1);
 
-    REQUIRE(contaPalavras(testeInicio) == expectedAswer);
+    REQUIRE(contaPalavras(fileName) == expectedAswer);
 }
